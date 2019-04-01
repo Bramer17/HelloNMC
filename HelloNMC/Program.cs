9c0407b94cb20d313a4819450f213dff2c78b27d
Bramer17 <46691158+Bramer17@users.noreply.github.com>
2019-04-01 13:57:32 -0400

    Add project files.

---
+++ HelloNMC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello NMC!");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
